ba7856e351d414cc347617ac39e98a8c3c80b5b0
David Tsofin <tsofindavid@gmail.com>
2025-11-18 14:34:38 +0100

    update

--- migrate.ts
+++ migrate.ts
@@ -1,9 +1,14 @@
-import AWS from "aws-sdk";
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new AWS.S3();
+import basex from "base-x";
+
+const ALPHABET = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+const base58 = basex(ALPHABET);
+
+const s3 = new S3Client();
 
 async function main() {
-  const b = s3.listObjects();
+  const b = await s3.send(new ListBucketsCommand());
 }
 
 main();
