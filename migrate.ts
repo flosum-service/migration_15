fb5508bfcf02757d29737385261e29d84dc1a6a0
David Tsofin <tsofindavid@gmail.com>
2025-11-18 14:41:16 +0100

    update

--- migrate.ts
+++ migrate.ts
@@ -1,4 +1,8 @@
-import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  ListBucketsCommand,
+  ListObjectsCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 
 import basex from "base-x";
 
@@ -8,7 +12,11 @@ const base58 = basex(ALPHABET);
 const s3 = new S3Client({ region: "us-east-2" });
 
 async function main() {
-  const b = await s3.send(new ListBucketsCommand());
+  const b = await s3.send(
+    new ListObjectsCommand({ Bucket: "dev-devops-us-east-2-artem-bucket" })
+  );
+
+  console.log(b);
 }
 
 main();
